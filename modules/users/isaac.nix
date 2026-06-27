162436384676052e27f46934982144c1afea1f04
Isaac-Pruett <isaacpruett@outlook.com>
2026-06-27 14:30:09 -0700

    Update isaac.nix

--- modules/users/isaac.nix
+++ modules/users/isaac.nix
@@ -7,12 +7,15 @@
       users.users.isaac = {
         isNormalUser = true;
         description = "Isaac";
-
+        shell = pkgs.zsh;
         extraGroups = [
           "wheel"
           "networkmanager"
         ];
         hashedPassword = "$y$j9T$347eNzn/hAlwANwJKIWt8/$VykLNHB6zt3uB2Z4d2VbrXDAIQZM/h4tBh65yGRHhV9";
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICu6nS96uOLf4wQ+W6Uncnjh276dffhewG9zxeqQ7YSi isaac"
+        ];
       };
 
       home-manager.users.isaac = {
